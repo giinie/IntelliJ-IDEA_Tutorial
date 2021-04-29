a357e61dc67fdeb1baf77eaa422d6bb301480fa1
giinie <giinie72@gmail.com>
2021-04-29 23:45:12 +0900

    Initial Commit

--- src/com/bjpublic/intellij/tutorial/Merge.java
+++ src/com/bjpublic/intellij/tutorial/Merge.java
@@ -4,5 +4,6 @@ public class Merge {
     public void merge() {
         System.out.println("[Merge] master branch <-> develop branch");
         System.out.println("master branch 에서 작업한 코드");
+        System.out.println("develop branch 에서 작업한 코드");
     }
 }
